77f61b7f92522bd7fc350cd00525a4487fff0d8d
magelon <alfardproject@gmail.com>
2015-07-24 14:58:38 -0700

    somechanges

---
+++ create_databaseforuser.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+if($dbc = @mysql_connect('localhost','root','1234')){
+	print '<p>Successfully connect to MySQL!</p>';
+	//try to creat database
+	if(@mysql_query('CREATE DATABASE users_id',$dbc)){
+		print '<p>The database has been created!</p>';
+	}else{
+		//could not create database
+		print '<p style="color: red;"> Could not create the database because:<br />'.mysql_error($bdc).'.</p>';
+	//try to select the database
+	if(@mysql_select_db('users_id',$dbc)){
+		print '<p>the database has been selected</p>';
+	}else{
+		print '<p style="color: red;">Could not select the database because:<br />'.
+		mysql_error($dbc).'.</p>';
+	}
+	
+	}
+}
+?>  
+
+</body>
+</html>
