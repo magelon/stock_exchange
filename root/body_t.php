255d77034b2ddab79f2bd45c40cfcd38c1aab55d
lon <alfardproject@gmail.com>
2015-08-24 23:21:45 -0700

    +10

--- root/body_t.php
+++ root/body_t.php
@@ -3,8 +3,11 @@
 
 echo "
 <div class=\"row\">
-<h1 class=\"col-md-4\">{$messages['subject']}  &nbsp; &nbsp; &nbsp;&nbsp; value:  {$messages['value']}<button type=\"button\" class=\"btn btn-primary\"><span class=\"glyphicon glyphicon-thumbs-up\" aria-hidden=\"true\"></span> </button>
+<h1 class=\"col-md-4\">{$messages['subject']}  &nbsp; &nbsp; &nbsp;&nbsp; value:{$messages['value']}
 </h1>
+<button type=\"button\" class=\"btn btn-primary\"><span class=\"glyphicon glyphicon-thumbs-up\" aria-hidden=\"true\"></span>
+</button>
+<div id=\"div1\" style=\"display: none;\">+10</div>
   <h2>
   <!-- value:  {$messages['value']}   value_each:-->
   </h2>
